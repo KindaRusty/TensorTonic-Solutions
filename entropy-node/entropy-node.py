a97d4a9e279b42e87de2e3ccd2549b219907c0d0
KindaRusty <thanhkien2900@gmail.com>
2026-03-04 10:02:23 +0700

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -4,12 +4,12 @@ def entropy_node(y):
     y = np.array(y)
     if len(y) == 0:
         return 0.0
-    
+    #get the total input and frequency
     classes, counts = np.unique(y, return_counts=True)
     probs = counts / len(y)
-    
+    #remove <0 prob
     nonzero = probs[probs > 0]
-    
+    #formula 
     h = -np.sum(nonzero * np.log2(nonzero))
     
     return float(h)
