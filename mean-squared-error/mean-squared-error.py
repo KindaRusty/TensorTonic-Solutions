65209967f6dbb9410e402fefd47063dec8f5b5a8
KindaRusty <thanhkien2900@gmail.com>
2026-03-14 22:40:26 +0700

    Update solution for mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -6,6 +6,6 @@ def mean_squared_error(pred, true):
     """
     pred = np.array(pred)
     true = np.array(true)
-
+    #tính tổng trung bình các giá trị
     mse = np.mean((pred-true)**2)
     return float(mse)
