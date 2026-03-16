eed1fdc333fcd53ea6d7bc472dd5a3cbf48e13ca
KindaRusty <thanhkien2900@gmail.com>
2026-03-16 09:24:23 +0700

    Update solution for softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -7,8 +7,13 @@ def softmax(x):
     For 2D, compute row-wise softmax.
     """
     x = np.array(x)
+    #tính e^x
     x_max = np.max(x, axis=-1, keepdims=True)
+    #tìm giá trị lớn nhất
     exp_x = np.exp(x - x_max)
+    #tính tổng của các giá trị mũ e
     sum_exp_x = np.sum(exp_x, axis=-1, keepdims=True)
+    #hàm tính softmax
+    #bình thường thì không có max nhưng mà khi code thì thêm vào để tránh số mũ lớn vd: mũ 1000,1001,1002 thì trừ 1002 còn mũ -2,-1,0 tính sẽ nhanh hơn và ko làm crash
     softmax = exp_x/sum_exp_x
     return softmax
