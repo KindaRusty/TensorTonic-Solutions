169a568d51aa955ed14dfbfdede8890c641b4e65
KindaRusty <thanhkien2900@gmail.com>
2026-03-16 10:25:06 +0700

    Update solution for td-value-update

--- td-value-update/td-value-update.py
+++ td-value-update/td-value-update.py
@@ -4,9 +4,11 @@ def td_value_update(V, s, r, s_next, alpha, gamma):
     """
     Returns: updated value function V_new
     """
-    v_new = np.array(V, dtype=float)
-    td_error = r + gamma * v_new[s_next] - v_new[s]
     
+    v_new = np.array(V, dtype=float)
+    #tính TD Error
+    td_error = r + gamma * v_new[s_next] - v_new[s]
+    #cập nhật giá trị dựa trên tốc độ học (learning rate)
     v_new[s] = v_new[s] + alpha * td_error
 
     return v_new
