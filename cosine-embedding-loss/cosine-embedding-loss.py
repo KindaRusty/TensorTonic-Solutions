5d97a8f345d42703d1f088236d8ac3959f3ec76a
KindaRusty <thanhkien2900@gmail.com>
2026-03-16 08:58:40 +0700

    Update solution for cosine-embedding-loss

--- cosine-embedding-loss/cosine-embedding-loss.py
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -1,11 +1,15 @@
 import math
+#ko dùng numpy
 def cosine_embedding_loss(x1, x2, label, margin):
     """
     Compute cosine embedding loss for a pair of vectors.
     """
+    #tính tích vô hướng (dot product)
     dot_product = sum(a*b for a,b in zip(x1, x2))
+    #tích độ dài vector (norms)
     norm_x1 = math.sqrt(sum(a * a for a in x1))
     norm_x2 = math.sqrt(sum(b * b for b in x2))
+    #tính cosine 
     cos = dot_product / (norm_x1 * norm_x2)
     if label == 1:
         return float(1-cos)
