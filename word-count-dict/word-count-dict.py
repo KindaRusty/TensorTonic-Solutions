5ec8d4cee2cd2998083fef61b2cb3cb0bbaebdcb
KindaRusty <thanhkien2900@gmail.com>
2026-03-17 11:40:30 +0700

    Update solution for word-count-dict

--- word-count-dict/word-count-dict.py
+++ word-count-dict/word-count-dict.py
@@ -10,7 +10,7 @@ def word_count_dict(sentences):
         for word in sentence:
             #3: kiểm tra 
             if word in my_dict:
-                my_dict[word] +=1
+                my_dict[word] +=1 #ko dùng string vì nó sẽ print ra chính đó luôn 
             else:
                 my_dict[word] = 1
     return my_dict
