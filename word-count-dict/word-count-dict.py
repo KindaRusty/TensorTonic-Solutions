c7561d8e260329761e94fd1b74c699c23297aa05
KindaRusty <thanhkien2900@gmail.com>
2026-03-17 11:39:59 +0700

    Add solution for word-count-dict

---
+++ word-count-dict/word-count-dict.py
@@ -0,0 +1,16 @@
+def word_count_dict(sentences):
+    """
+    Returns: dict[str, int] - global word frequency across all sentences
+    """
+    my_dict = {}
+    
+    #1: Lặp từng câu
+    for sentence in sentences:
+        #2: lặp từng từ
+        for word in sentence:
+            #3: kiểm tra 
+            if word in my_dict:
+                my_dict[word] +=1
+            else:
+                my_dict[word] = 1
+    return my_dict
