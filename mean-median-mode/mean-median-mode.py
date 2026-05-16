93f0d7b39edec8f85be75192602f7af258970449
KindaRusty <thanhkien2900@gmail.com>
2026-05-16 00:08:45 -0700

    Update solution for mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -1,15 +1,15 @@
 import numpy as np
 from collections import Counter
 
-def mean_median_mode(x):
+def mean_median_mode(x): #x là dãy số
     """
     Compute mean, median, and mode.
     """
-    mean = np.mean(x)
-    median = np.median(x)
-    
-    freq = Counter(x)
-    max_freq = max(freq.values())
+    mean = np.mean(x) #tính trung bình 
+    median = np.median(x) #tính trung vị 
+    #tính mốt
+    freq = Counter(x) #tính tần suất 
+    max_freq = max(freq.values()) #tìm kiếm giá trị có tần suất xuất hiện nhiều nhất
     modes = [key for key, value in freq.items() if value == max_freq]
     
     mode_value = min(modes) 
