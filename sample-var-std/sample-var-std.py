4c67a1d7c7837de832dc24dfc14894dc507e954b
KindaRusty <thanhkien2900@gmail.com>
2026-05-18 18:38:05 -0700

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -5,10 +5,10 @@ def sample_var_std(x):
     Compute sample variance and standard deviation.
     """
     x = np.array(x)
-    mean_of_x = np.mean(x)
-    n = len(x) 
+    mean_of_x = np.mean(x) #tính giá trị trung bình
+    n = len(x) #Tính độ dài dãy số
     
-    variance = np.sum((x - mean_of_x) ** 2) / (n - 1)
+    variance = np.sum((x - mean_of_x) ** 2) / (n - 1) #tính phương sai
 
-    std = np.sqrt(variance)
-    return variance, std
+    std = np.sqrt(variance) #tính sai lệch
+    return variance, std #in kết quả
