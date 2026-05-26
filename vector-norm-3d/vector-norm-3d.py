7303f65db0f0b65172e43b5dea7194a48eff4e26
KindaRusty <thanhkien2900@gmail.com>
2026-05-25 19:10:36 -0700

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -4,6 +4,8 @@ def vector_norm_3d(v):
     """
     Compute the Euclidean norm of 3D vector(s).
     """
-    v = np.array(v)
-    magnitude = np.linalg.norm(v, axis=-1)
+    #dùng asarray sẽ tiết kiệm bộ nhớ 1 chút nếu v đã là array sẵn rồi
+    v = np.asarray(v)
+    #np.linalg.norm với axis=-1 sẽ tự động trả về một số thực (float scalar) nếu đầu vào là vector 1D, và trả về mảng shape (N,) nếu đầu vào là 2D
+    magnitude = np.linalg.norm(v, axis=-1) #axis=-1 sẽ tính toán chiều cuối cùng phụ thuộc vào input là vector đơn lẻ hay là 1 batch
     return magnitude
