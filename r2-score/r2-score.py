cf3d7b779e82429a97100884ec85a00e0706b9c2
KindaRusty <thanhkien2900@gmail.com>
2026-05-29 19:03:09 -0700

    Update solution for r2-score

--- r2-score/r2-score.py
+++ r2-score/r2-score.py
@@ -9,17 +9,10 @@ def r2_score(y_true, y_pred) -> float:
     """
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
-    # 1. Get mean value
-    y_mean = np.mean(y_true)
-    # 2. Handle constant
-    if np.all(y_true == y_mean):
-        if np.array_equal(y_true, y_pred):
-            return 1.0
-        else:
-            return 0.0
-    # 3. Calculate
-    ss_res = np.sum(np.square(y_true - y_pred)) #Có thể dùng **2 là bình phương thay vì dùng np.square
-    ss_tot = np.sum(np.square(y_true - y_mean))
     
-    r2 = 1 - (ss_res / ss_tot)
-    return float(r2)
+    y_mean = np.mean(y_true)
+    SStot = np.sum((y_true - y_mean)**2)
+    SSres = np.sum((y_true - y_pred)**2)
+    if SStot == 0:
+        return 1.0 if SSres == 0 else 0.0
+    return 1 - (SSres / SStot)
